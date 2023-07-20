32f2f764682f3d391a799a350e3495433cf7f7be
Jayalaxmi Routray <132996345+Jayalaxmi7@users.noreply.github.com>
2023-07-20 15:28:23 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'booking',
+    loadChildren: () => import('./booking/booking.module').then( m => m.BookingPageModule)
   },
+
 ];
 
 @NgModule({
