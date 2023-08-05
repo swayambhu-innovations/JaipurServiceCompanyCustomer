18eff607e80ad6685ca77e3ca8a89fd83053ca1c
Jayalaxmi Routray <132996345+Jayalaxmi7@users.noreply.github.com>
2023-08-05 16:14:19 +0530

    Profile main page completed and routing set

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  constructor(
+    private router: Router,
+    private modalCtrl: ModalController,
+    public navCtrl: NavController
+  ) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  
+  close() {
+    this.router.navigate(['/home']);
+  }
+  job() {
+    this.router.navigate(['job-history']);
+  }
+  financial() {
+    this.router.navigate(['/financial-details']);
+  }
+  editProfile() {
+    this.router.navigate(['/edit-profile']);
   }
 
+  signout() {
+    this.router.navigate(['/signout']);
+  }
+  logout() {
+    this.router.navigate(['/profile']);
+    this.modalCtrl.dismiss();
+  }
+
+  closeModal() {
+    this.modalCtrl.dismiss();
+  }
 }
