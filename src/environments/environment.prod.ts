292ffbb6c0dd500a523bf98137263a6057297023
Pratap Singh Sisodia <48864550+prataps135@users.noreply.github.com>
2023-09-09 19:06:22 +0530

    android app created.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,13 @@ export const environment = {
     messagingSenderId: '681472195786',
     measurementId: 'G-RCFP3GVMRL',
   },
+  cloudFunctions : {
+    createOrder: 'http://localhost:5001/jaipurservicecompany/us-central1/createOrder',
+    capturePayment: 'http://localhost:5001/jaipurservicecompany/us-central1/capturePayments',
+    createSubscription: 'http://localhost:5001/jaipurservicecompany/us-central1/createSubscription',
+    verifySubscription:'http://localhost:5001/jaipurservicecompany/us-central1/verifySubscription',
+    checkSubscriptionStatus:'http://localhost:5001/jaipurservicecompany/us-central1/checkSubscriptionStatus',
+  },
+  RAZORPAY_KEY_ID: 'rzp_test_8cTBlk022y2EDq',
   production: true
 };
