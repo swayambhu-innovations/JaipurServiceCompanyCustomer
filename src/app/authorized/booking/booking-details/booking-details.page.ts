f398da7a297d697b1d2de624cc0b58587ee9063d
Anukul <anukul1299@gmail.com>
2024-01-12 12:49:53 +0530

    payment ui

--- src/app/authorized/booking/booking-details/booking-details.page.ts
+++ src/app/authorized/booking/booking-details/booking-details.page.ts
@@ -520,10 +520,8 @@ export class BookingDetailsPage implements OnInit {
             content: [
                 'First paragraph',
                 'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
-            ]
-            
+            ]    
         }
         return dd;
     }
-
 }
