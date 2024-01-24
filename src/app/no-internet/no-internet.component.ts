bed31281c5eba590e6a9785ac2238b90ddf76747
shivamtyagi102 <shivamtyagi102@gmail.com>
2024-01-24 17:53:29 +0530

    no internet back

--- src/app/no-internet/no-internet.component.ts
+++ src/app/no-internet/no-internet.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Network } from '@capacitor/network';
 import { NavigationBackService } from '../navigation-back.service';
+import { Platform } from '@ionic/angular';
+import { App } from '@capacitor/app';
 @Component({
   selector: 'app-no-internet',
   templateUrl: './no-internet.component.html',
@@ -13,12 +15,23 @@ export class NoInternetComponent  implements OnInit {
   showmodal: boolean = true;
   canDismiss:boolean = false;
   @ViewChild('modal') modal;
-  constructor(private router: Router,private _navigationService: NavigationBackService) {
+  constructor(
+    private router: Router,
+    private _navigationService: NavigationBackService,
+    private platform: Platform,
+    ) {
   }
 
   ngOnInit() {
     
   }
+
+  onWillDismiss(event){
+    if(this.platform.is('cordova') || this.platform.is('mobile')){
+      App.exitApp();
+    }
+  }
+
   ionViewDidEnter(){
     this.checkStatus = true;
     this.modal.present();
