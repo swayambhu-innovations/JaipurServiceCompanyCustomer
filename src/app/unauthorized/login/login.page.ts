d6433e2ca5b322fabe3867bdd7bf156aa68498d4
Srikant <srikanthraj1779@gmail.com>
2024-01-26 20:05:11 +0530

    updates

--- src/app/unauthorized/login/login.page.ts
+++ src/app/unauthorized/login/login.page.ts
@@ -14,6 +14,7 @@ import { LoadingController } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
   phoneNumber:string= '';
+  terms:boolean= false;
   verifier:RecaptchaVerifier|undefined;
   constructor(private router: Router,private authService:AuthService,public dataProvider:DataProviderService,private alertify:AlertsAndNotificationsService,private loaderService:LoadingController) { }
 
