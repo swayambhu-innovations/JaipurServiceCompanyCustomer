352197afaba2200052d8d74367ee9d1cc827ed13
Shivam <shivamtyagi102@gmail.com>
2024-02-07 17:28:19 +0530

    Bug: Location getting loader

--- src/app/authorized/new-address/new-address.page.ts
+++ src/app/authorized/new-address/new-address.page.ts
@@ -214,7 +214,7 @@ export class NewAddressPage implements OnInit, CanActivate{
         else{
           
         }
-        loader.present();
+        loader.dismiss();
         this.setPointerOutside();
         
       })
