21384ccba0029616356ea735925d26355df5f0d6
Chinmay Samal <104675814+chinus1998@users.noreply.github.com>
2024-07-20 10:51:15 +0530

    restricted country search

--- src/app/fetch-address/search-loc/search-loc.page.ts
+++ src/app/fetch-address/search-loc/search-loc.page.ts
@@ -13,7 +13,7 @@ import { NavController } from '@ionic/angular';
 export class SearchLocPage implements OnInit {
   private storage = 'Storage';
   searchInputSubject: Subject<string> = new Subject<string>();
-
+  recentSearches: any[] = [];
   serviceList: any[] = [];
   fuseSearchInstance = new Fuse(this.serviceList, {
     keys: ['name', 'variants.name'],
@@ -38,10 +38,18 @@ export class SearchLocPage implements OnInit {
         if (term.length > 2) {
           const geocoder = new google.maps.Geocoder();
           geocoder
-            .geocode({ address: term })
+            .geocode({
+              address: term,
+              componentRestrictions: {
+                country: 'IN',
+              },
+            })
             .then((res) => {
               const { results } = res;
               this.results = [...results];
+              this.results.forEach((area) => {
+                area.geometry.location = area.geometry.location.toJSON();
+              });
               this.resultsFetched = true;
             })
             .catch((err) => {
@@ -56,88 +64,47 @@ export class SearchLocPage implements OnInit {
       });
   }
 
-  ionViewDidLoad() {
+  ionViewDidEnter() {
+    this.recentSearches.length = 0;
     this.resultsFetched = false;
+    let recentLoc = localStorage.getItem('recent-loc');
+    if (recentLoc) this.recentSearches = [...JSON.parse(recentLoc)];
   }
 
   currLoc() {
     this.router.navigate(['/fetch-address']);
   }
 
+  deleteLoc(selectedArea: any) {
+    this.recentSearches = this.recentSearches.filter((area) => {
+      return area.place_id !== selectedArea.place_id;
+    });
+  }
+
   sendLoc(selectedArea: any) {
-    let coord = selectedArea.geometry.location.toJSON();
+    const coord = selectedArea.geometry.location;
+    this.recentSearches.push(selectedArea);
+    this.recentSearches = this.recentSearches.filter(
+      (value, index, self) =>
+        index === self.findIndex((t) => t.place_id === value.place_id)
+    );
+    localStorage.removeItem('recent-loc');
+    localStorage.setItem('recent-loc', JSON.stringify(this.recentSearches));
     this.router.navigate(['/fetch-address/gps-map', coord]);
   }
 
   ionViewWillLeave() {
+    localStorage.removeItem('recent-loc');
+    localStorage.setItem('recent-loc', JSON.stringify(this.recentSearches));
     this.results = [];
+    this.recentSearches = [];
     this.resultsFetched = false;
     this.inputSearchVar = '';
   }
 
-  ngOnInit() {
-    this.dataProvider.mainCategories.subscribe((mainCategory) => {
-      let areas: any[] = [];
-
-      this.fuseSearchInstance.setCollection(areas);
-      this.serviceList = areas;
-    });
-  }
+  ngOnInit() {}
 
   goBack() {
     this.router.navigateByUrl('fetch-address');
-  }
-
-  saveToHistory(term: string) {
-    if (term.length <= 0) return;
-    let data = JSON.parse(localStorage.getItem('searchedTerms') || '{}');
-    if (typeof data.terms == 'object' && data.terms.length >= 0) {
-      data.terms.push(term);
-      localStorage.setItem('searchedTerms', JSON.stringify(data));
-    } else {
-      localStorage.setItem('searchedTerms', JSON.stringify({ terms: [term] }));
-    }
-  }
-
-  openService(service: any) {
-    let subCatId = '';
-
-    this.dataProvider.mainCategories.subscribe((mainCategory) => {
-      mainCategory.forEach((mainCategory) => {
-        let subCat = mainCategory.subCategories.filter((subCategory: any) => {
-          let serviceFind = subCategory.services.filter(
-            (serviced) => serviced.id === service.id
-          );
-          return serviceFind.length > 0 ? true : false;
-        });
-        if (subCat.length > 0) {
-          subCatId = subCat[0].id;
-        }
-      });
-      if (service?.mainCatId && subCatId) {
-        this.router.navigate([
-          '/authorized/service-detail/' +
-            service?.mainCatId +
-            '/' +
-            subCatId +
-            '/' +
-            service.id,
-        ]);
-      }
-      this.resultsFetched = false;
-    });
-  }
-  getFromHistory(): string[] {
-    let data = JSON.parse(localStorage.getItem('searchedTerms') || '{}');
-    if (typeof data.terms == 'object' && data.terms.length >= 0) {
-      return data.terms.reverse();
-    } else {
-      return [];
-    }
-  }
-  removeItemFromHistory(index: number) {
-    let data = JSON.parse(localStorage.getItem('searchedTerms') || '{}');
-    data.terms.splice(index, 1);
-    localStorage.setItem('searchedTerms', JSON.stringify(data));
   }
 }
