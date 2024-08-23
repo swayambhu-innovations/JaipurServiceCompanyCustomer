45681b63b5364ca535a8975fb6823e79d2483749
KritarthShukla <143200473+KritarthShukla@users.noreply.github.com>
2024-08-23 18:12:00 +0530

    fix-ui-pop/23/kritarth

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -121,7 +121,7 @@ export class AuthService {
       if (!this.dataProvider.currentUser && !this.isLoginPage) {
         await this.openLoginModal();
       }
-    }, 600); // 2 minutes
+    }, 60000); // 2 minutes
   }
 
   private async openLoginModal() {
