fbb33ae0c37df911df46fc4e42fa22060613733e
KritarthShukla <143200473+KritarthShukla@users.noreply.github.com>
2024-08-24 17:57:46 +0530

    login-pop-desktop

--- src/app/widgets/login-popup-d/login-popup-d.component.ts
+++ src/app/widgets/login-popup-d/login-popup-d.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login-popup-d',
@@ -9,23 +9,26 @@ import { Router } from '@angular/router';
 })
 export class LoginPopupDComponent  implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private modalCtrl: ModalController) { }
   isOpen :boolean;
 
   ngOnInit() {}
 
   closeModal() {
     this.isOpen=false;
+    this.modalCtrl.dismiss();
   }
 
   skipLogin() {
     this.isOpen=false;
+    this.modalCtrl.dismiss();
     // this.router.navigate(['home'])
   }
 
   login() {
    
     this.router.navigate(['/unauthorized/jsc-logo']);
+    this.modalCtrl.dismiss({ loggedIn: true });
     this.isOpen=false;
      
   }
